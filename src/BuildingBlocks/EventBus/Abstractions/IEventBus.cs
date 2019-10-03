05e2544d74329c348598f4df670f0361c30045c9
Arsen Yutuc <arsen@weareblis.com>
2019-10-03 11:56:41 +0800

    the eventbus

---
+++ src/BuildingBlocks/EventBus/Abstractions/IEventBus.cs
@@ -0,0 +1,22 @@
+﻿using EventBus.Events;
+
+namespace EventBus.Abstractions
+{
+    public interface IEventBus
+    {
+        void Publish(IntegrationEvent @event);
+
+        void Subscribe<T, TH>()
+            where T : IntegrationEvent
+            where TH : IIntegrationEventHandler<T>;
+
+        void SubscribeDynamic<TH>(string eventName)
+            where TH : IDynamicIntegrationEventHandler;
+
+        void UnSubscribeDynamic<TH>(string eventName)
+            where TH : IDynamicIntegrationEventHandler;
+        void UnSubscribe<T, TH>()
+            where T : IntegrationEvent
+            where TH : IIntegrationEventHandler<T>;
+    }
+}
---
+++ src/BuildingBlocks/EventBus/IEventBusSubscriptionsManager.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using EventBus.Abstractions;
+using EventBus.Events;
+using static EventBus.InMemoryEventBusSubscriptionsManager;
+
+namespace EventBus
+{
+    public interface IEventBusSubscriptionsManager
+    {
+        bool IsEmpty { get; }
+        event EventHandler<string> OnEventRemoved;
+
+        void AddDynamicSubscription<TH>(string eventName)
+            where TH : IDynamicIntegrationEventHandler;
+
+        void AddSubscription<T, TH>()
+            where T : IntegrationEvent
+            where TH : IIntegrationEventHandler<T>;
+
+        void RemoveSubscription<T, TH>()
+            where T : IntegrationEvent
+            where TH : IIntegrationEventHandler<T>;
+
+        void RemoveDynamicSubscription<TH>(string eventName)
+            where TH : IDynamicIntegrationEventHandler;
+
+        bool HasSubscriptionsForEvent<T>() where T : IIntegrationEvent;
+        bool HasSubscriptionsForEvent(string eventName);
+        Type GetEventTypeByName(string eventName);
+        void Clear();
+        IEnumerable<SubscriptionInfo> GetHandlersForEvent<T>() where T : IntegrationEvent;
+        IEnumerable<SubscriptionInfo> GetHandlersForEvent(string eventName);
+        string GetEventKey<T>();
+    }
+}
